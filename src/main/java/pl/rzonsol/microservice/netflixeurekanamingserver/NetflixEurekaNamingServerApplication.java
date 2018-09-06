9d81e1ecba5ce84eb9ce1687bfcb6470fd6a168d
rzonsol <rzonsol@gmail.com>
2018-09-06 10:25:13 +0200

    Initial commit

---
+++ src/main/java/pl/rzonsol/microservice/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,12 @@
+package pl.rzonsol.microservice.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaNamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+    }
+}
